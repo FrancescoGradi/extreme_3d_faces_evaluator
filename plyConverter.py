1b2dff1ac0d58e7e15aec645d95a6d7194c1924e
freaky1310@gmail.com <arthas1310>
2019-07-16 13:30:04 +0200

    Revert "correct uniform sampling implemented"
    This reverts commit 980266c7

--- plyConverter.py
+++ plyConverter.py
@@ -1,10 +1,11 @@
 from os import listdir
 from os.path import isfile, join
 from utils import is_in_range
-from utils import uniform_sampling
+
+import math
 
 
-def plyTotxt(directory, compression_level=1):
+def plyTotxt(directory, compressionLevel=12, radius=25):
 
     files = [f for f in listdir(directory) if isfile(join(directory, f))]
 
@@ -12,7 +13,37 @@ def plyTotxt(directory, compression_level=1):
 
         print("Processing file --> " + filename)
 
-        uniform_sampling(directory, filename, compression_level)
+        with open(directory + filename, 'rb') as ply:
+
+            txt = open("data/" + filename[:-4] + ".txt", "w+")
+
+            if b'ply' not in ply.readline():
+                raise ValueError('The file does not start whith the word ply')
+
+            lines = ply.readlines()
+
+            i = 0
+            j = 0
+
+            for line in lines:
+                if i < 9:
+                    i += 1
+                    continue
+
+                elif line.split()[0] is not b'3' and (j % compressionLevel) == 0:
+                    text = str(line)
+                    text = text[2:-3]
+                    coordinates = text.split(sep=" ")
+
+                    if is_in_range(radius, x=(float)(coordinates[0]), y=(float)(coordinates[1]),
+                                 z=(float)(coordinates[2])):
+                        txt.write(text + '\n')
+
+                j += 1
+
+
+compressionLevel = 16
+radius = 70
 
 directory = 'filePlyRete/'
-plyTotxt(directory, compression_level=20)
+#plyTotxt(directory, compressionLevel, radius)
